a22a7c0ab2b118b4866597a888fd9b187b4441a5
House197 <101058821+Houser97@users.noreply.github.com>
2025-02-18 15:45:52 -0600

    Set up payments-microservice

---
+++ src/main.ts
@@ -0,0 +1,33 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { envs } from './config/envs';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
+async function bootstrap() {
+  const logger = new Logger('Payments Microservice');
+
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true
+    })
+  )
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: envs.natsServers
+    }
+  }, {
+    inheritAppConfig: true
+  })
+
+  await app.listen(envs.port);
+  logger.log(`Payments Microservice running on port ${envs.port}`)
+}
+bootstrap();
